c2c7b535269f113846a46713c2d3487866be2ddf
constantijn <constantijn@vanhartesveldt.nl>
2022-04-13 23:24:05 +0200

    added meta data to video

--- frontend/pages/[...videoid].js
+++ frontend/pages/[...videoid].js
@@ -3,16 +3,26 @@ import Comment from "../components/comment";
 import UrlToId from "../lib/urltoid";
 import Header from "../components/header";
 
-export default function Detail({data}) {
+export default function Detail(props) {
   // console.log(api_dumpert)
   // console.log(api_db)
+  console.log(props);
 
   return (
     <>
-      <Header/>
-      {data.map((comment) => (
-        <Comment key={comment.id} props={comment}/>
-      ))}
+      <Header />
+      <>
+        <div>
+          <div>
+            locked?: {props.api_dumpert.summary.can_comment ? "yes" : "no"}
+          </div>
+          <div>comments: {props.api_dumpert.summary.comment_count}</div>
+          <div>last moderated: {props.api_dumpert.summary.moderated_at}</div>
+        </div>
+        {props.comments.map((comment) => (
+          <Comment key={comment.id} props={comment} />
+        ))}
+      </>
     </>
   );
 }
@@ -22,30 +32,35 @@ export async function getServerSideProps(ctx) {
   const db = (await clientPromise).db().collection("comments");
 
   // formats to a video id as described by the api
-  const video_id = UrlToId(ctx.query)
+  const video_id = UrlToId(ctx.query);
 
   let api_dumpert;
-  const dumpert_id = video_id.replace("_","/")
-  await fetch(`https://comments.dumpert.nl/api/v1.1/articles/${dumpert_id}/comments/?includeitems=1`).then(res => res.json()).then(res => api_dumpert = res.data.comments)
+  const dumpert_id = video_id.replace("_", "/");
+  await fetch(
+    `https://comments.dumpert.nl/api/v1.1/articles/${dumpert_id}/comments/?includeitems=1`
+  )
+    .then((res) => res.json())
+    .then((res) => (api_dumpert = res));
 
   const api_db = await db
     .find({ article_id: video_id }, { projection: { _id: 0 } })
     .toArray();
 
-  let data = [];
+  let comments = [];
 
-  api_dumpert.forEach((key) => {
+  api_dumpert.data.comments.forEach((key) => {
     if (key.display_content === "-weggejorist-") {
       const chached_comment = api_db.find((p) => {
         return p.id === key.id;
       });
-      
-      // check if comment is scraped 
-      chached_comment ? (key.original_content = chached_comment.display_content) : (key.original_content = "-hasnt been scraped yet-")
 
+      // check if comment is scraped
+      chached_comment
+        ? (key.original_content = chached_comment.display_content)
+        : (key.original_content = "-hasnt been scraped yet-");
     }
-    data.push(key);
+    comments.push(key);
   });
 
-  return { props: { data } };
+  return { props: { comments, api_dumpert } };
 }
