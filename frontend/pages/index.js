10ce0ce7fbf9ef7aba3f9ef89e42d66e4d34b94a
constantijn <constantijn@vanhartesveldt.nl>
2022-04-14 10:38:40 +0200

    updated frontpage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,51 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useRouter } from 'next/router'
+import style from "../styles/Home.module.css";
+import Header from "../components/header";
+const { URL } = process.env;
 
-export default function Home() {
+export default function Home(props) {
   return (
     <>
-      <div>please replace dumpert.nl to localhost:3000 to see removed comments</div>
+      <Header />
+      <div className={style.center}>
+        <div>
+          please replace dumpert.nl to {props.URL} to see removed comments
+        </div>
+        <p>what is this site?</p>
+        <p>
+          by continus scraping of dumpert i am able to see and save
+          comments/videos before they are removed
+        </p>
+        <p>
+          when a user sees a removed comment he can replace dumpert.nl to <br />
+          {props.URL} and see the removed comments by a left join from dumpert
+          and my database
+        </p>
+        <p>
+
+          example: 
+          <br/>
+          https://www.dumpert.nl/item/100026920_f0089181/
+          <br />
+          to 
+          <br />
+          {props.URL}/item/100026920_f0089181/
+          <br/>
+          or
+          <br/>
+          {props.URL}/item?selectedId=100027744_d54603bb
+        </p>
+        <p>
+          are you from telegraaf? please lookup the repo to find my contact
+          details
+        </p>
+      </div>
     </>
-  )
+  );
+}
+
+export async function getStaticProps(ctx) {
+  // returns the base url so the url is not hard coded
+  return {
+    props: { URL },
+  };
 }
