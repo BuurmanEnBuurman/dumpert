3b7be71fab38ee3468d8d38a80f44e2f6553eba3
constantijn <constantijn@vanhartesveldt.nl>
2022-05-06 14:36:37 +0200

    working on VideoMeta graphql

--- frontend/pages/api/graphql.js
+++ frontend/pages/api/graphql.js
@@ -1,14 +1,36 @@
 import { gql, ApolloServer } from "apollo-server-micro";
+import clientPromise from "../../lib/mongodb"
 
 const typeDefs = gql`
   type User {
     id: ID
   }
 
+  type VideoMediaStills {
+    still: String
+    still_large: String
+    still_medium: String
+    thumb: String
+    thumb_medium: String
+  }
+
+  type VideoMediaStats {
+    kudos_today: Int
+    kudos_total: Int
+    views_today: Int
+    views_total: Int
+  }
+
+  type VideoMediaVariants {
+    uri: String
+    version: String
+  }
+
   type VideoMedia {
-    description
-    duration
-    mediatype
+    description: String
+    duration: Int
+    mediatype: String
+    variants: [VideoMediaVariants]
   }
 
   type Video {
@@ -26,6 +48,9 @@ const typeDefs = gql`
     title: String
     upload_id: ID
     upload_date: String
+    media: [VideoMedia]
+    stats: VideoMediaStats
+    stills: VideoMediaStills
   }
 
   type Query {
@@ -41,7 +66,11 @@ const resolvers = {
         id: "ID",
       };
     },
-    getVideoMeta: () => {
+    getVideoMeta: async (_, args) => {
+      const db = (await clientPromise).db()
+      const data = await db.collection("videos").findOne({})
+      console.log(args)
+
       return {
         id: "Foo",
         article_id: "ID",
@@ -57,6 +86,27 @@ const resolvers = {
         title: "String",
         upload_id: "ID",
         upload_date: "String",
+        media: [
+          {
+            description: "kaas",
+            duration: 2,
+            mediatype: "video",
+            variants: [{ uri: "uri", version: "mobile" }],
+          },
+        ],
+        stats: {
+          kudos_today: 123,
+          kudos_total: 4,
+          views_today: 3,
+          views_total: 2,
+        },
+        stills: {
+          still: "https://media.dumpert.nl/...",
+          still_large: "https://media.dumpert.nl/...",
+          still_medium: "https://media.dumpert.nl/..",
+          thumb: "https://media.dumpert.nl/...",
+          thumb_medium: "https://media.dumpert.nl/...",
+        },
       };
     },
   },
